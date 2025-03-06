61dc9c5a3522ac9d8b0b2c23cd79707313033dd4
danikk2004 <antifreeze2004@mail.ru>
2025-03-06 11:42:01 +0300

    change  frame in meshed object

--- include/objects/meshed.h
+++ include/objects/meshed.h
@@ -6,20 +6,18 @@
 
 namespace sotiria_project
 {
-	class MeshedObject : virtual Object
+	class MeshedObject : virtual WorldObject
 	{
 		Mesh mesh;
 		Texture texture;
+		vec4 color = vec4(1.0f);
 
 		void frame(Object& parent) override
 		{
-			glUniformMatrix4fv(shader_layout_locations::world, 1, 0, (float*)&world);
-			glUniformMatrix2fv(shader_layout_locations::uv, 1, 0, (float*)&uv);
-			glUniform4fv(shader_layout_locations::color, 1, (float*)&color);
-			texture = texture ? texture : white_texture.container->texture_data.handle;
-			glActiveTexture(GL_TEXTURE0);
-			glBindTexture(GL_TEXTURE_2D, texture);
-			mesh.bind();
+			setWorldMatrix(world_matrix);
+			setDrawColor(color);
+			uint texture_handle = !texture.isNull() ? texture.container->texture_data.handle : 0;
+			bindTexture(texture_handle, 0);
 			mesh.draw();
 			Object::frame(parent);
 		}
